bdeaa05053d7b88650c1aa2fd7415549d2c58720
Bikas Basnet <143680053+Bikasbasnettest@users.noreply.github.com>
2024-04-21 21:35:28 +0545

    Add files via upload

--- Relative abundenc plot in R.R
+++ Relative abundenc plot in R.R
@@ -16,14 +16,75 @@ Taxononmy
 DAT<- DAT %>%
   left_join(Taxononmy, by = "otu_id")
 DAT
+
 ###now we are gonna a sample metadeta file addtion likewise same the 
-SM<- read_tsv("E:/Relative abundance plot in R/sample_metadata.tsv")#,
-                 # col_types = cols(.default = col_character()))
-SM
+Meta<-read_tsv("E:/Relative abundance plot in R/sample_metadata.tsv",
+              col_types = cols(.default = col_character()))
+                
+Meta
 colnames(SM)
 
 ##3#now add the new column
 DAT <- DAT %>%
-  left_join(SM, by = "otu_id")
+  left_join(Meta, by = c("Sample_id" = "sample_id"))
 
+DAT
+
+
+##33# separation of the sample by fractions and types of the soil
+DAT %>%
+  ggplot(aes(x = Sample_id, y = count)) +
+  facet_grid(~ fraction + soil, scales = "free_x", space = "free_x") +
+  geom_bar(aes(fill = Phylum.y), stat = "identity", position = "fill", width = 1)
+####paring of the data sets
+DAT %>%
+  ggplot(aes(x = Sample_id, y = count)) +
+  facet_grid(~ fraction + soil, scales = "free_x", space = "free_x") +
+  geom_bar(aes(fill = Phylum.y), stat = "identity", position = "fill", width = 1) +
+  scale_fill_brewer(palette = "Paired")
+
+##3to order the phylum based on the realtive abundance of the species 
+phyla_order <- c("Proteobacteria",
+                 "Actinobacteria",
+                 "Bacteroidetes",
+                 "Acidobacteria",
+                 "Firmicutes",
+                 "Cyanobacteria",
+                 "Verrucomicrobia",
+                 "Gemmatimonadetes",
+                 "Armatimonadetes",
+                 "Chloroflexi",
+                 "unclassified")
+DAT <- DAT %>%
+  mutate(Phylum = factor(Phylum.y, levels = phyla_order))
+#3#3final plot 
+DAT %>%
+  ggplot(aes(x = Sample_id, y = count)) +
+  facet_grid(~ fraction + soil, scales = "free_x", space = "free_x") +
+  geom_bar(aes(fill = Phylum.y), stat = "identity", position = "fill", width = 1) +
+  scale_fill_brewer(palette = "Paired") +
+  scale_y_continuous(name = "Relative abundance of the bacterial species",
+                     labels = scales::percent) +
+  theme(axis.text.x = element_text(angle = 45, size = 10),
+        axis.text.y = element_text(color = "black"),
+        strip.text = element_text(face = "bold"),
+        strip.background = element_blank())
+##3to set the color by manual
+standard_colors <- c("maroon", "#ff7f0e", "navy", "magenta", "red", "darkgreen", 
+                     "springgreen", "yellow", "cyan", "purple", "orange", "blue")
+
+RAD<-DAT %>%
+  ggplot(aes(x = Sample_id, y = count, fill = Phylum.y)) +
+  facet_grid(~ fraction + soil, scales = "free_x", space = "free_x") +
+  geom_bar(stat = "identity", position = "fill", width = 1) +
+  scale_fill_manual(values = standard_colors) +  
+  scale_y_continuous(name = "Relative abundance of the bacterial species",
+                     labels = scales::percent) +
+  theme(axis.text.x = element_text(angle = 45, size = 10),
+        axis.text.y = element_text(color = "black"),
+        strip.text = element_text(face = "bold"),
+        strip.background = element_blank())
+RAD
+getwd()
+ggsave("Relative abundenc plot of bacteria species plot.png", RAD, width = 15, height = 12, dpi = 800)
 
